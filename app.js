2dc8232a16d53c5d655d97a237aa9f9e483f11e6
AngheloM <macha.anghelo@gmail.com>
2018-02-14 14:31:55 -1000

    Created Fortunes Array

---
+++ app.js
@@ -0,0 +1,12 @@
+var Fortunes = [
+"A fresh start will put you on your way.",
+"A friend asks only for your time not your money.",
+"A friend is a present you give yourself.",
+"A gambler not only will lose what he has, but also will lose what he doesn’t have.",
+"A golden egg of opportunity falls into your lap this month.",
+"A good friendship is often more important than a passionate romance.",
+"A hunch is creativity trying to tell you something.",
+"A lifetime friend shall soon be made.",
+"A lifetime of happiness lies ahead of you.",
+"A light heart carries you through all the hard times."
+]
